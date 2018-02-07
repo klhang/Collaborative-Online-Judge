bce478e5612798cd17e51dbdd6223042e8f3051e
Klhang <chrisjunhang@gmail.com>
2018-02-07 12:40:00 -0800

    add getProblems getProblems(id), addProblem Routes

--- OnlineJudge-server/routes/rest.js
+++ OnlineJudge-server/routes/rest.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 const problemService = require('../services/problemService')
 
@@ -7,5 +9,23 @@ router.get('/problems', function(req, res) {
   problemService.getProblems()
     .then(problems => res.json(problems));
 });
+
+router.get('/problems/:id', (req, res) => {
+  const id = req.params.id;
+  problemService.getProblem(+id)
+    .then(problem => res.json(problem));
+})
+
+router.post('/problems', jsonParser, (req, res) => {
+  problemService.addProblem(req.body)
+    .then(
+      (problem) => {
+        res.json(problem);
+      },
+      (error) => {
+        res.status(400).send("Problem already exists");
+      }
+    )
+})
 
 module.exports = router;
--- OnlineJudge-server/services/problemService.js
+++ OnlineJudge-server/services/problemService.js
@@ -32,9 +32,29 @@ let problems = [{
 const getProblems = function() {
   return new Promise((resolve, reject) => {
     resolve(problems);
+  });
+}
+
+const getProblem = function(id) {
+  return new Promise((resolve, reject) => {
+    resolve(problems.find(problem => problem.id === id));
+  });
+}
+
+const addProblem = function(newProblem) {
+  return new Promise((resolve, reject) => {
+    if (problems.find(problem => problem.name === newProblem.name)) {
+      reject('Prblem already exists');
+    } else {
+      newProblem.id = problems.length + 1;
+      problems.push(newProblem);
+      resolve(newProblem);
+    }
   })
 }
 
 module.exports = {
-  getProblems
+  getProblems,
+  getProblem,
+  addProblem
 }
